20be637d1ab6f29c855843a91c689ba3703bcc36
spudwoot <149447428+spudwoot@users.noreply.github.com>
2024-04-17 14:54:09 -0600

    Update Connection3.h

--- Connection3.h
+++ Connection3.h
@@ -1,23 +1,80 @@
-const char *HTML_CONTENT_Connection3 = R"rawliteral(
-<!DOCTYPE html>
-<html lang="en">
+const char *HTML_CONTENT_HOME = R"rawliteral(
+<!DOCTYPE HTML><html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" href="data:,">
-    <title>Configuration Settings</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <style>
+    html {
+     font-family: Arial;
+     display: inline-block;
+     margin: 0px auto;
+     text-align: center;
+    }
+    h2 { font-size: 3.0rem; }
+    p { font-size: 3.0rem; }
+    .units { font-size: 1.2rem; }
+    .dht-labels{
+      font-size: 1.5rem;
+      vertical-align:middle;
+      padding-bottom: 15px;
+    }
+  </style>
 </head>
 <body>
-    <h2>Password</h2>
-    <br>
-    <h1>Enter the password of the service account</h1>
- 
-
-
-<form action="/get">
-password: <input type="text" name="spassword">
-<input type="submit" value="Submit">
-</form><br>
-
+  <h2>Current Monitor Status</h2>
+  <p>
+    <i class="fas fa-thermometer-half" style="color:#059e8a;"></i> 
+    <span class="dht-labels">Temperature</span> 
+    <span id="temperature">%TEMPERATURE%</span>
+    <sup class="units">&deg;F</sup>
+  </p>
+  <p>
+    <i class="fas fa-tint" style="color:#00add6;"></i> 
+    <span class="dht-labels">Humidity</span>
+    <span id="humidity">%HUMIDITY%</span>
+    <sup class="units">&percnt;</sup>
+  </p>
+    <p>
+    <i class="fas fa-wifi" style="color:#00add6;"></i> 
+    <span class="dht-labels">Mac</span>
+    <span id="mac">%MAC%</span>
+  </p>
+  <p>
+  <h1><li><a href="/Connection.html">Set up Service Now Rest API</a></li></h1>
+  </P>
 </body>
+<script>
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("temperature").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/temperature", true);
+  xhttp.send();
+}, 10000 ) ;
+
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("humidity").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/humidity", true);
+  xhttp.send();
+}, 10000 ) ;
+
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("mac").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/mac", true);
+  xhttp.send();
+}, 10000 ) ;
+</script>
 </html>)rawliteral";
